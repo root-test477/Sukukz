08345571cb618bc7b93d9b01b981b8fbc96f5426
root-test477 <cs.reply.maersk@gmail.com>
2025-05-15 19:23:17 -0700

    Delete src directory

--- src/bot.ts
+++
@@ -1,6 +0,0 @@
-import TelegramBot from 'node-telegram-bot-api';
-import * as process from 'process';
-
-const token = process.env.TELEGRAM_BOT_TOKEN!;
-
-export const bot = new TelegramBot(token, { polling: true });
