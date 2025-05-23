c9d280d60eb9d0d0bad34409982715a461dfa900
root-test477 <cs.reply.maersk@gmail.com>
2025-05-23 13:19:28 -0700

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
-  moduleNameMapper: {
-    '^src/(.*)$': '<rootDir>/src/$1',
-  },
-  setupFilesAfterEnv: ['<rootDir>/src/tests/test-setup.ts'],
-};
